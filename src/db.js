8f68c76af8e6faf1eb0cc1c217cc515fb55568c2
SRohithkanna <srohithkanna@gmail.com>
2026-04-09 22:41:02 +0530

    ready for railway deployment

--- src/db.js
+++ src/db.js
@@ -1,17 +1,27 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT, 
-  waitForConnections: true,
-  connectionLimit: 10,
-  ssl: {
-    rejectUnauthorized: false 
-  }
-});
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
+let pool;
+
+if (process.env.DATABASE_URL) {
+  // ✅ Railway (production)
+  pool = mysql.createPool({
+    uri: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+} else {
+  // ✅ Local (development)
+  pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  });
+}
 
 module.exports = pool;
